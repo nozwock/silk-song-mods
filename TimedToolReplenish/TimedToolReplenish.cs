c35e26ff8fa6a1c6d4de19849e4472d37e16750d
nozwock <57829219+nozwock@users.noreply.github.com>
2025-09-10 04:57:44 +0530

    Bump version to 1.1.1

--- TimedToolReplenish/TimedToolReplenish.cs
+++ TimedToolReplenish/TimedToolReplenish.cs
@@ -12,7 +12,7 @@ public class PluginInfo
 {
     public const string PLUGIN_GUID = "nozwock.TimedToolReplenish";
     public const string PLUGIN_NAME = "Timed Tool Replenish";
-    public const string PLUGIN_VERSION = "1.1.0";
+    public const string PLUGIN_VERSION = "1.1.1";
 }
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
