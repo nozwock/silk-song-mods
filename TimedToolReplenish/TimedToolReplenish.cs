d3ad961cd0df09a914aa9c9c76fdc10f772a9bb3
nozwock <57829219+nozwock@users.noreply.github.com>
2025-09-14 03:13:29 +0530

    Bump version to 1.3.1

--- TimedToolReplenish/TimedToolReplenish.cs
+++ TimedToolReplenish/TimedToolReplenish.cs
@@ -15,7 +15,7 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PLUGIN_GUID = "nozwock.TimedToolReplenish";
     public const string PLUGIN_NAME = "Timed Tool Replenish";
-    public const string PLUGIN_VERSION = "1.3.0";
+    public const string PLUGIN_VERSION = "1.3.1";
 
     internal static ManualLogSource Log;
 
